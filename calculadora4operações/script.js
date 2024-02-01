489c9792329c3ffb81e67a2f12e937a67f8abc34
lucasgoissenai <lucas.gois@docente.fieto.br>
2024-02-01 16:33:23 -0300

    correção

--- calculadora4operações/script.js
+++ calculadora4operações/script.js
@@ -1,20 +1,20 @@
 // Função para somar dois números
-function somar(a, b) {
-  return a + b;
+function somar(numeroA, numeroB) {
+  return numeroA + numeroB;
 }
 
 // Função para subtrair dois números
-function subtracao(a, b) {
+function subtrair(a, b) {
   return a - b;
 }
 
 // Função para multiplicar dois números
-function multiplicacao(a, b) {
+function multiplicar(a, b) {
   return a * b;
 }
 
 // Função para dividir dois números
-function divisao(a, b) {
+function dividir(a, b) {
   return a / b;
 }
 
---
+++ calculadora4operações/styles.css
@@ -0,0 +1,13 @@
+p {
+    color: red;
+    font-family: 'Times New Roman', Times, serif;
+    font-size: 20px;
+}
+
+body {
+    background-image: url("https://wallpapercave.com/wp/wp2497638.jpg");
+    align-content: center;
+}
+h1 {
+    text-align: center;
+}
